f96524f2b8e1ee44a1d78ba8914a371784eee11f
Jalmari Ikävalko <tzaeru@gmail.com>
2019-09-20 18:18:26 +0300

    Better docker-compose setup for development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,34 @@
 import * as Koa from 'koa';
 import * as Router from '@koa/router';
+import * as Knex from 'knex';
 
 const app = new Koa();
 const router = new Router();
 
+const options: object = {
+    client: 'postgres',
+    connection: {
+    	user: 'postgres',
+    	password: 'postgres',
+        host: 'postgres',
+    }
+}
+const knex = new (Knex as any)(options);
+
 router.get('/*', async (ctx) => {
     ctx.body = 'Hello World!';
 });
+
+knex.schema.createTable('cars', (table: any) => {
+    table.increments('id')
+    table.string('name')
+    table.integer('price')
+}).then(() => console.log("table created"))
+    .catch((err: any) => { console.log(err); throw err })
+    .finally(() => {
+        knex.destroy();
+    });
+
 
 app.use(router.routes());
 
